16d5a9898a5baa25273db3d5b757aff30ad2b5ff
qiutiandong <qiutiandong@gmail.com>
2017-10-08 13:55:23 +0800

    hello world example

---
+++ spring-boot-helloword/src/main/java/com/dongjun/springboothelloword/SpringBootHellowordApplication.java
@@ -0,0 +1,12 @@
+package com.dongjun.springboothelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHellowordApplication.class, args);
+	}
+}
