6ba160a37c56bc88470a747e7e464d187de12f06
DongJun <qiutiandong@gmail.com>
2017-10-11 14:30:41 +0800

    添加事物注解

--- spring-boot-swagger2/src/main/java/com/dongjun/springbootswagger2/domain/UserRepository.java
+++ spring-boot-swagger2/src/main/java/com/dongjun/springbootswagger2/domain/UserRepository.java
@@ -1,12 +1,14 @@
 package com.dongjun.springbootswagger2.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
     User findById(Integer id);
 
+    @Transactional
     void deleteById(Integer id);
 
 }
